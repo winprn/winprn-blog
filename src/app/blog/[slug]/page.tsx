811a6d10ca677b0458b3b16581383ea4a6e7cc59
winprn <minhladz001@gmail.com>
2023-05-25 15:01:12 +0700

    generate static paths

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,5 +1,12 @@
-import { getPostContent } from "@/utils";
+import { getPostContent, getPostsInfo } from "@/utils";
 import Markdown from "markdown-to-jsx";
+
+export async function generateStaticParams() {
+    const posts = getPostsInfo();
+    return posts.map((post) => ({
+        slug: post.slug,
+    }));
+}
 
 const ViewPost = (props: any) => {
     return <main className="max-w-2xl mx-auto overflow-auto">
